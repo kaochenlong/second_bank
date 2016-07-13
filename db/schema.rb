a6e0c6b804849d979f0aaed8161008a6ebb5fa63
Eddie Kao <eddie@digik.com.tw>
2016-07-13 09:51:36 +0800

    add bank account model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160125072150) do
+ActiveRecord::Schema.define(version: 20160713014752) do
+
+  create_table "bank_accounts", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "example_users", force: :cascade do |t|
     t.string   "name"
