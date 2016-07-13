c17bad6305272928527821341af0340f85eece72
Eddie Kao <eddie@digik.com.tw>
2016-07-13 10:16:42 +0800

    finish model test

--- app/models/bank_account.rb
+++ app/models/bank_account.rb
@@ -1,2 +1,13 @@
 class BankAccount < ActiveRecord::Base
+  def 存錢(n)
+    update_attributes(amount: self.amount + n) if n.positive?
+  end
+
+  def 領錢(n)
+    update_attributes(amount: self.amount - n) if n.positive?
+  end
+
+  def 餘額
+    amount
+  end
 end
